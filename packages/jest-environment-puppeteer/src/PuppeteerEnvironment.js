0fd5b19f6e6679836b9ad2fd7eb0e6e197bdd000
Greg Bergé <berge.greg@gmail.com>
2018-04-01 16:34:26 +0000

    fix(jest-environment-puppeteer): support slowMo
    Fixes #20

--- packages/jest-environment-puppeteer/src/PuppeteerEnvironment.js
+++ packages/jest-environment-puppeteer/src/PuppeteerEnvironment.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import NodeEnvironment from 'jest-environment-node'
 import puppeteer from 'puppeteer'
+import readConfig from './readConfig'
 import { WS_ENDPOINT_PATH } from './constants'
 
 const handleError = error => {
@@ -9,11 +10,13 @@ const handleError = error => {
 
 class PuppeteerEnvironment extends NodeEnvironment {
   async setup() {
+    const config = await readConfig()
     const wsEndpoint = fs.readFileSync(WS_ENDPOINT_PATH, 'utf8')
     if (!wsEndpoint) {
       throw new Error('wsEndpoint not found')
     }
     this.global.browser = await puppeteer.connect({
+      slowMo: config && config.launch && config.launch.slowMo ? config.launch.slowMo : undefined,
       browserWSEndpoint: wsEndpoint,
     })
     this.global.page = await this.global.browser.newPage()
