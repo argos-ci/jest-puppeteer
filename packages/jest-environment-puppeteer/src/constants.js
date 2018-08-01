358f0c3b1853621885acbba6148409eb66bf135c
Seun Ogedengbe <SEUN_OGEDENGBE@REYREY.COM>
2018-08-01 15:42:05 -0500

    Use PID based wsEndpoint file
    Allows multiple Jest processes to connect to their own chromium instances

--- packages/jest-environment-puppeteer/src/constants.js
+++ packages/jest-environment-puppeteer/src/constants.js
@@ -2,4 +2,4 @@ import path from 'path'
 import os from 'os'
 
 export const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup')
-export const WS_ENDPOINT_PATH = path.join(DIR, 'wsEndpoint')
+export const WS_ENDPOINT_PATH = path.join(DIR, 'wsEndpoint-' + process.pid)
