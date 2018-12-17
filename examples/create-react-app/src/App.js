06b57e15c0d9d993d599189f7b8782c2a7d64cab
xiaoyuhen <xiaoyuhen1994@gmail.com>
2018-12-18 01:51:38 +0800

    update cra example

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -9,6 +9,20 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <button className="App-button">Get Started</button>
+          <input name="textInput" className="App-input" size="32" />
+          <form className="App-form" name="testForm">
+            <label for="App-testOne">test1:</label>
+            <input className="App-testInput" type="text" name="testOne" />
+            <label for="App-testTwo">test2:</label>
+            <input className="App-testInput" type="text" name="testTwo" />
+          </form>
+          <select name="testSelect">
+            <option value="first">First Value</option>
+            <option value="second" selected>Second Value</option>
+            <option value="third">Third Value</option>
+          </select>
+          <input name="textInput" className="App-inputFile" size="32" type="file" />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
