f43159820861ea73cef486db4fc9a26a28032f74
Tony Brix <tony@brix.ninja>
2019-01-15 09:21:40 -0600

    improve test

--- packages/jest-environment-puppeteer/tests/resetPage-2.test.js
+++
@@ -1,8 +0,0 @@
-describe('resetPage', () => {
-  test('resetPage should reset page', async () => {
-    await jestPuppeteer.resetPage()
-    const text = await page.$("#test");
-    expect(text).toBe(null)
-    await page.setContent('<div id="test"></div>')
-  })
-})
