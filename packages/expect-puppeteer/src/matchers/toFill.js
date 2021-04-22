593913ef6eefdc735abe0c00828cede9f33d937d
Tony Brix <tony@brix.ninja>
2021-04-21 20:55:50 -0500

    fix: fix toFill on textarea

--- packages/expect-puppeteer/src/matchers/toFill.js
+++ packages/expect-puppeteer/src/matchers/toFill.js
@@ -1,5 +1,29 @@
 import toMatchElement from './toMatchElement'
 
+async function selectAll(element) {
+  // modified from https://github.com/microsoft/playwright/issues/849#issuecomment-587983363
+  await element.evaluate((elementHandle) => {
+    if (elementHandle.setSelectionRange) {
+      try {
+        elementHandle.setSelectionRange(0, elementHandle.value.length)
+      } catch (e) {
+        // setSelectionRange throws an error for inputs: number/date/time/etc
+        // we can just focus them and the content will be selected
+        elementHandle.focus()
+      }
+    } else if (window.getSelection && document.createRange) {
+      const range = document.createRange()
+      range.selectNodeContents(elementHandle)
+
+      const selection = window.getSelection()
+      if (selection) {
+        selection.removeAllRanges()
+        selection.addRange(range)
+      }
+    }
+  })
+}
+
 async function toFill(instance, selector, value, options) {
   const { delay, ...toMatchElementOptions } = options || {}
   const element = await toMatchElement(
@@ -7,7 +31,7 @@ async function toFill(instance, selector, value, options) {
     selector,
     toMatchElementOptions,
   )
-  await element.click({ clickCount: 3 })
+  await selectAll(element)
   await element.press('Backspace')
   await element.type(value, {
     delay,
