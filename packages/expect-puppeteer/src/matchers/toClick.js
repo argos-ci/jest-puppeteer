986ab1226fe055494fcca6e7e53b80827c449faa
Jan Peer Stöcklmair <jan.oster94@gmail.com>
2021-05-05 13:22:40 +0200

    fix: click selector differently to prevent invisible Node

--- packages/expect-puppeteer/src/matchers/toClick.js
+++ packages/expect-puppeteer/src/matchers/toClick.js
@@ -1,8 +1,11 @@
 import toMatchElement from './toMatchElement'
+import { getContext } from '../utils'
 
 async function toClick(instance, selector, options) {
-  const element = await toMatchElement(instance, selector, options)
-  await element.click(options)
+  const { page } = await getContext(instance, () => document)
+
+  await toMatchElement(instance, selector, options)
+  await page.evaluate((selector) => document.querySelector(selector).click(), selector)
 }
 
 export default toClick
