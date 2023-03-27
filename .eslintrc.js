69f3d78cb676ab3f690e1b13b6e725f4ffcda6a7
Colin Rotherham <work@colinr.com>
2023-03-27 13:37:21 +0100

    feat: add ESLint plugins for Jest, TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,25 @@ module.exports = {
     {
       files: ["*.test.?(m|t)js"],
       env: {
-        jest: true,
+        "jest/globals": true,
       },
+      plugins: ["jest"],
+    },
+    {
+      files: ["*.ts"],
+      extends: "plugin:@typescript-eslint/recommended",
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: [
+          "./tsconfig.json",
+          "./packages/expect-puppeteer/tsconfig.json",
+          "./packages/jest-dev-server/tsconfig.json",
+          "./packages/jest-environment-server/tsconfig.json",
+          "./packages/jest-puppeteer/tsconfig.json",
+        ],
+        tsconfigRootDir: __dirname,
+      },
+      plugins: ["@typescript-eslint"],
     },
   ],
 };
